1c988d252fbbefba93174410f94a8bfaf7f7adce
Ostap Sulyk <ostapsulyk@gmail.com>
2022-05-15 12:21:17 -0400

    refactored code get_date and get_amount funcitons to actually return values

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use inquire::{ui::RenderConfig, CustomType, DateSelect};
 //
 
 fn get_date() {
-    struct Today{
+    struct Today {
         year: i32,
         month: u32,
         day: u32,
@@ -19,7 +19,7 @@ fn get_date() {
     impl Today {
         fn new() -> Today {
             let now = chrono::Utc::now();
-            Today{
+            Today {
                 year: now.year(),
                 month: now.month(),
                 day: now.day(),
@@ -27,27 +27,29 @@ fn get_date() {
         }
     }
     let today = Today::new();
+
     let date = DateSelect::new("When expense happened?")
-    .with_default(NaiveDate::from_ymd(today.year, today.month, today.day))
-        .with_min_date(NaiveDate::from_ymd(today.year -1, today.month, today.day))
-        .with_max_date(NaiveDate::from_ymd(today.year +1, today.month, today.day))
+        .with_default(NaiveDate::from_ymd(today.year, today.month, today.day))
+        .with_min_date(NaiveDate::from_ymd(today.year - 1, today.month, today.day))
+        .with_max_date(NaiveDate::from_ymd(today.year + 1, today.month, today.day))
         .prompt();
-    
+
+    let date = date.unwrap();
+
 }
-fn get_amount() {
+fn get_amount() -> f64 {
     let amount_prompt = CustomType::<f64>::new("Enter amount: ")
         .with_formatter(&|i| format!("${:.2}", i))
         .with_error_message("Please type a valid number")
         .with_help_message("type amount in us dollars using . as separator")
         .prompt();
 
-    let amount:f64 = amount_prompt.unwrap_or_default();
-    println!("{}", amount);
-
-
+    let amount: f64 = amount_prompt.unwrap_or_default();
+    amount
 }
 
 fn main() {
-    get_date()
+    get_date();
+    get_amount();
 }
 //TODO get all expenses
