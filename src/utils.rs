0e3fb974b9104772260e2dbeb61e80dc515bec53
ostap_sulyk <ostapsulyk@gmail.com>
2022-05-15 14:50:12 -0400

    extracted code from main to external utils file

---
+++ src/utils.rs
@@ -0,0 +1,40 @@
+use inquire::{ui::RenderConfig, CustomType, DateSelect};
+use chrono::prelude::*;
+use chrono::NaiveDate;
+
+pub fn get_date() {
+    struct Today {
+        year: i32,
+        month: u32,
+        day: u32,
+    }
+    impl Today {
+        fn new() -> Today {
+            let now = chrono::Utc::now();
+            Today {
+                year: now.year(),
+                month: now.month(),
+                day: now.day(),
+            }
+        }
+    }
+    let today = Today::new();
+
+    let date = DateSelect::new("When expense happened?")
+        .with_default(NaiveDate::from_ymd(today.year, today.month, today.day))
+        .with_min_date(NaiveDate::from_ymd(today.year - 1, today.month, today.day))
+        .with_max_date(NaiveDate::from_ymd(today.year + 1, today.month, today.day))
+        .prompt();
+
+    let date = date.unwrap();
+}
+pub fn get_amount() -> f64 {
+    let amount_prompt = CustomType::<f64>::new("Enter amount: ")
+        .with_formatter(&|i| format!("${:.2}", i))
+        .with_error_message("Please type a valid number")
+        .with_help_message("type amount in us dollars using . as separator")
+        .prompt();
+
+    let amount: f64 = amount_prompt.unwrap_or_default();
+    amount
+}
